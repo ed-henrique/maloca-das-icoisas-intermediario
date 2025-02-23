b9cb4c58fedd652a8930e2d62e003c0f80d657b3
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2025-02-23 00:03:21 -0400

    ci(web): add Dockerfile

--- web/app.py
+++ web/app.py
@@ -5,21 +5,24 @@ from firebase_admin import credentials, firestore
 from datetime import datetime
 import re
 import pytz
+import os
 
-utc=pytz.UTC
+utc = pytz.UTC
 
 # Inicialização do Flask
 app = Flask(__name__)
 
 # Inicialização do Firebase Admin
-cred = credentials.Certificate(r"C:\Users\ALUNOS MALOCA\Downloads\maloca intermediario\maloca-intermediario-firebase-adminsdk-fbsvc-d8709d321d.json")
+cred = credentials.Certificate(
+    r"/app/maloca-intermediario-firebase-adminsdk-fbsvc-222aac05ef.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 # Configurações do MQTT
-MQTT_BROKER = '172.22.68.36'
+MQTT_BROKER = os.environ['MOSQUITTO']
 MQTT_PORT = 1883
 MQTT_TOPIC = 'pacientes_pos_cirurgicos/monitoramento'
+
 
 def esta_de_cabeca_para_baixo(x, y, z):
     margem_erro = 0.05
@@ -28,6 +31,7 @@ def esta_de_cabeca_para_baixo(x, y, z):
         return True
     else:
         return False
+
 
 def on_message(client, userdata, msg):
     try:
@@ -47,7 +51,7 @@ def on_message(client, userdata, msg):
         paciente_id = str(payload.get('paciente', 1))
 
         dados_atualizados = {
-            'timestamp':firestore.SERVER_TIMESTAMP,
+            'timestamp': firestore.SERVER_TIMESTAMP,
             'movimento_x': payload.get('movimento_x'),
             'movimento_y': payload.get('movimento_y'),
             'movimento_z': payload.get('movimento_z'),
@@ -58,19 +62,23 @@ def on_message(client, userdata, msg):
         conds = [
             dados_atualizados['batimento_cardiaco'] > 130,
             dados_atualizados['temperatura'] > 37.8,
-            esta_de_cabeca_para_baixo(dados_atualizados['movimento_x'], dados_atualizados['movimento_y'], dados_atualizados['movimento_z']),
+            esta_de_cabeca_para_baixo(
+                dados_atualizados['movimento_x'], dados_atualizados['movimento_y'], dados_atualizados['movimento_z']),
         ]
 
         if True in conds:
             client.publish(MQTT_TOPIC, b"ALERT")
 
         # Atualiza ou cria o documento do paciente no Firestore
-        db.collection('pacientes').document(paciente_id).collection('dados').add(dados_atualizados)
+        db.collection('pacientes').document(
+            paciente_id).collection('dados').add(dados_atualizados)
     except Exception as e:
         print(f"Erro ao processar mensagem MQTT: {e}")
 
+
 def on_publish(client, userdata, mid):
     print(f"Message {mid} published.")
+
 
 # Inicializa e conecta o cliente MQTT
 mqtt_client = mqtt.Client()
@@ -81,31 +89,36 @@ mqtt_client.subscribe(MQTT_TOPIC)
 mqtt_client.loop_start()
 
 # Rota para buscar dados do paciente com filtragem por período
+
+
 @app.route('/pacientes', methods=['GET'])
 def get_pacientes():
     try:
         paciente_id = '1'
         periodo_inicio = request.args.get('periodo_inicio')
         periodo_fim = request.args.get('periodo_fim')
-        
+
         if not paciente_id:
             return jsonify({'erro': 'Parâmetro "paciente" é obrigatório.'}), 400
         if not periodo_inicio or not periodo_fim:
             return jsonify({'erro': 'Parâmetros "periodo_inicio" e "periodo_fim" são obrigatórios.'}), 400
-        
+
         # Busca o documento do paciente
-        doc_ref = db.collection('pacientes').document(str(paciente_id)).collection('dados')
+        doc_ref = db.collection('pacientes').document(
+            str(paciente_id)).collection('dados')
         doc = doc_ref.get()
 
-        periodo_inicio_novo = utc.localize(datetime.fromisoformat(periodo_inicio))
+        periodo_inicio_novo = utc.localize(
+            datetime.fromisoformat(periodo_inicio))
         periodo_fim_novo = utc.localize(datetime.fromisoformat(periodo_fim))
-        
+
         filtered = []
         for record in doc:
             ts = record.get('timestamp')
             if isinstance(ts, datetime) and periodo_inicio_novo <= ts <= periodo_fim_novo:
-                filtered.append(record.to_dict())  # Converte timestamp para string ISO
-        
+                # Converte timestamp para string ISO
+                filtered.append(record.to_dict())
+
         dado_retorno = filtered
 
         print(dado_retorno)
@@ -113,6 +126,7 @@ def get_pacientes():
 
     except Exception as e:
         return jsonify({'erro': f'Erro ao buscar dados: {e}'}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
